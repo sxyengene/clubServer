62032409069d7e020bb93b09d68a23c45b19e229
sxyengene <sxyengene@163.com>
2019-01-02 14:11:21 +0800

    service

---
+++ app/service/user.js
@@ -0,0 +1,22 @@
+const Service = require('egg').Service;
+class UserService extends Service {
+  // 默认不需要提供构造函数。
+  // constructor(ctx) {
+  //   super(ctx); 如果需要在构造函数做一些处理，一定要有这句话，才能保证后面 `this.ctx`的使用。
+  //   // 就可以直接通过 this.ctx 获取 ctx 了
+  //   // 还可以直接通过 this.app 获取 app 了
+  // }
+  async findById(uid) {
+    // 假如 我们拿到用户 id 从数据库获取用户详细信息
+    const user = await this.ctx.model.User.findOne({id:uid});
+    console.log(user);
+    return user;
+  }
+
+  async getAllInfo(uid){
+    const user = await this.ctx.model.User.findOne({id:uid});
+    let userInfo = await this.getAllInfo(uid);
+    return userInfo;
+  }
+}
+module.exports = UserService;
