dd81534327e6fa862ff47199c0e71b8211187220
sxyengene <sxyengene@163.com>
2019-01-24 15:23:44 +0800

    course

--- src/app/controller/courses.js
+++ src/app/controller/courses.js
@@ -8,9 +8,28 @@ module.exports = app => {
     }
 
     async allCourses(ctx) {
-      const result = await ctx.service.course.allCourses();
+      let result = {};
+      result.list  = await ctx.service.course.allCourses();
       this.ctx.body = result;
     }
+
+    // async allCourses(ctx) {
+    //   let query = ctx.query,user;
+    //   if(query.openid){
+    //     user = await ctx.service.user.findByOpenid(query.openid);
+    //     if(user){
+    //       let result = {};
+    //       result.list  = await ctx.service.course.allCourses();
+    //       this.ctx.body = result;
+    //     }else{
+    //       ctx.body = 'need login';
+    //       return;
+    //     }
+    //   }else{
+    //     ctx.body = 'need login';
+    //     return;
+    //   }
+    // }
   }
   return CoursesController;
 };
--- src/app/service/course.js
+++ src/app/service/course.js
@@ -12,23 +12,23 @@ class CourseService extends Service {
     const courses = await this.ctx.model.Course.findAll();
     return courses;
   }
-  //
+
   // async findByName(name) {
   //   console.log('findByName');
   //   const user = await this.ctx.model.User.findOne({ where: { name } });
   //   return user;
   // }
-  //
+
   // async findByOpenid(openid) {
   //   console.log('findByName');
   //   const user = await this.ctx.model.User.findOne({ where: { openid } });
   //   return user;
   // }
-  //
-  // async upsertUser(params) {
-  //   const user = await this.ctx.model.User.upsert(params);
-  //   return user;
-  // }
+
+  async upsertUser(params) {
+    const user = await this.ctx.model.Course.upsert(params);
+    return user;
+  }
 
 }
 module.exports = CourseService;
