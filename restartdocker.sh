27d039b4bb994c4bfee68baad39c34ffd2f574bf
sxyengene <sxyengene@163.com>
2019-01-29 14:05:28 +0800

    com

--- restartdocker.sh
+++ restartdocker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git pull
 
-docker ps | grep sxyengene/clubserver | xargs docker rm -f
+docker ps | grep sxyengene/clubserver | awk {'print $1'} | xargs docker rm -f
 
 docker build -t sxyengene/clubserver .
 
-docker run -d -p 7001:7001 -v /root/git/clubServer/src/logs/:/var/www/html/clubServer/src/logs/ sxyengene/clubserver
+docker run -d -p 7001:7001 -v /root/git/clubServer/src/logs/:/var/www/html/clubServer/src/logs/ -v /etc/localtime:/etc/localtime sxyengene/clubserver
 #docker run -d -p 7001:7001 -v ./src/logs:/logs sxyengene/clubserver
